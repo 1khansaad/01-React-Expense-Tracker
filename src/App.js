2a5b626dd7571e9afc9ca7321f8a4b00ed040d9a
1khansaad <saadkhan8268@gmail.com>
2022-12-15 20:11:16 +0530

    created date component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
 import "./App.css";
-import ExpenseItem from "./expense-components/ExpenseItem";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const dummyData = [
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amount: 1234,
+      date: new Date(2021, 8, 28),
+    },
+    {
+      id: "e2",
+      title: "Books",
+      amount: 1234,
+      date: new Date(2021, 8, 28),
+    },
+    {
+      id: "e3",
+      title: "Food",
+      amount: 1234,
+      date: new Date(2021, 8, 28),
+    },
+    {
+      id: "e4",
+      title: "party",
+      amount: 1234,
+      date: new Date(2021, 8, 28),
+    },
+  ];
   return (
     <div className="App">
-      <ExpenseItem></ExpenseItem>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={dummyData[0].title}
+        amount={dummyData[0].amount}
+        date={dummyData[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={dummyData[1].title}
+        amount={dummyData[1].amount}
+        date={dummyData[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={dummyData[2].title}
+        amount={dummyData[2].amount}
+        date={dummyData[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={dummyData[3].title}
+        amount={dummyData[3].amount}
+        date={dummyData[3].date}
+      ></ExpenseItem>
     </div>
   );
 }
---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,15 @@
+import "./ExpenseDate.css";
+
+function ExpenseDate(props) {
+  const month = props.date.toLocaleString("en-us", { month: "long" });
+  const day = props.date.toLocaleString("en-us", { day: "2-digit" });
+  const year = props.date.getFullYear();
+  return (
+    <div className="expense-date">
+      <div>{day}</div>
+      <div>{month}</div>
+      <div>{year}</div>
+    </div>
+  );
+}
+export default ExpenseDate;
