0acd7da90afa6de65c432c93e3b521396cd2819b
1khansaad <saadkhan8268@gmail.com>
2023-01-29 22:02:04 +0530

    added state changer function for each input

--- expense-tracker/src/components/NewExpense/ExpenseForm.js
+++ expense-tracker/src/components/NewExpense/ExpenseForm.js
@@ -1,21 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const [currentTitle, setNewTitle] = useState("");
+  const [currentAmount, setNewAmount] = useState("");
+  const [currentDate, setNewDate] = useState("");
+
+  const TitleChangeHandler = (e) => {
+    setNewTitle(e.target.value);
+  };
+  const AmountChangeHandler = (e) => {
+    setNewAmount(e.target.value);
+  };
+  const DateChangeHandler = (e) => {
+    setNewDate(e.target.value);
+  };
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={TitleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={AmountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={DateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
