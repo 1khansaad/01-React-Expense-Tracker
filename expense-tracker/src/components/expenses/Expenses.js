2be6971988f6aafcb84ca33266543e1b84d61895
1khansaad <saadkhan8268@gmail.com>
2023-01-31 22:52:44 +0530

    up lifted filter selected data from filtered file to expenses file

--- expense-tracker/src/components/expenses/Expenses.js
+++ expense-tracker/src/components/expenses/Expenses.js
@@ -1,31 +1,44 @@
+import React, { useState } from "react";
 import "./Expenses.css";
 import ExpenceItem from "./ExpenceItem";
 import Card from "../UI/Crad";
+import ExpensesFilter from "../ExpenseFilter/ExpensesFilter";
 
 function Expenses(props) {
+  const [filteredYear, setFilterdYear] = useState("2023");
+
+  const yearChangeHandler = (year) => {
+    setFilterdYear(year);
+  };
   return (
-    <Card className="expenses">
-      <ExpenceItem
-        title={props.data[0].title}
-        amount={props.data[0].amount}
-        date={props.data[0].date}
-      />
-      <ExpenceItem
-        title={props.data[1].title}
-        amount={props.data[1].amount}
-        date={props.data[1].date}
-      />
-      <ExpenceItem
-        title={props.data[2].title}
-        amount={props.data[2].amount}
-        date={props.data[2].date}
-      />
-      <ExpenceItem
-        title={props.data[3].title}
-        amount={props.data[3].amount}
-        date={props.data[3].date}
-      />
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          setYear={filteredYear}
+          onGetDataHandler={yearChangeHandler}
+        />
+        <ExpenceItem
+          title={props.data[0].title}
+          amount={props.data[0].amount}
+          date={props.data[0].date}
+        />
+        <ExpenceItem
+          title={props.data[1].title}
+          amount={props.data[1].amount}
+          date={props.data[1].date}
+        />
+        <ExpenceItem
+          title={props.data[2].title}
+          amount={props.data[2].amount}
+          date={props.data[2].date}
+        />
+        <ExpenceItem
+          title={props.data[3].title}
+          amount={props.data[3].amount}
+          date={props.data[3].date}
+        />
+      </Card>
+    </div>
   );
 }
 
