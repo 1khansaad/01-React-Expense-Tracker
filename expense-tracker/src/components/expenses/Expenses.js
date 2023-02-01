215b9b934ed0a1acc77c518620f64461db900d7e
1khansaad <saadkhan8268@gmail.com>
2023-02-02 00:08:13 +0530

    outputting data based on conditions

--- expense-tracker/src/components/expenses/Expenses.js
+++ expense-tracker/src/components/expenses/Expenses.js
@@ -5,28 +5,34 @@ import Card from "../UI/Crad";
 import ExpensesFilter from "../ExpenseFilter/ExpensesFilter";
 
 function Expenses(props) {
-  const [filteredYear, setFilterdYear] = useState("2020");
+  const [year, setFilterdYear] = useState("2020");
 
   const yearChangeHandler = (year) => {
     setFilterdYear(year);
   };
+
+  const filteredArray = props.data.filter((x) => {
+    return String(x.date.getFullYear()) === year;
+  });
+
+  let expensesData = <p>No data found!</p>;
+  if (filteredArray.length > 0) {
+    expensesData = filteredArray.map((expense) => {
+      return (
+        <ExpenceItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      );
+    });
+  }
   return (
     <div>
       <Card className="expenses">
-        <ExpensesFilter
-          setYear={filteredYear}
-          onGetDataHandler={yearChangeHandler}
-        />
-        {props.data.map((expense) => {
-          return (
-            <ExpenceItem
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-          );
-        })}
+        <ExpensesFilter setYear={year} onGetDataHandler={yearChangeHandler} />
+        {expensesData}
       </Card>
     </div>
   );
