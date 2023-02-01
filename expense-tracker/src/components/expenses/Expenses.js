2d1d6f2b1d74c98d4e96fc4f2d762eee3f85c168
1khansaad <saadkhan8268@gmail.com>
2023-02-01 19:40:22 +0530

    rendered data dynamically using map

--- expense-tracker/src/components/expenses/Expenses.js
+++ expense-tracker/src/components/expenses/Expenses.js
@@ -17,26 +17,15 @@ function Expenses(props) {
           setYear={filteredYear}
           onGetDataHandler={yearChangeHandler}
         />
-        <ExpenceItem
-          title={props.data[0].title}
-          amount={props.data[0].amount}
-          date={props.data[0].date}
-        />
-        <ExpenceItem
-          title={props.data[1].title}
-          amount={props.data[1].amount}
-          date={props.data[1].date}
-        />
-        <ExpenceItem
-          title={props.data[2].title}
-          amount={props.data[2].amount}
-          date={props.data[2].date}
-        />
-        <ExpenceItem
-          title={props.data[3].title}
-          amount={props.data[3].amount}
-          date={props.data[3].date}
-        />
+        {props.data.map((expense) => {
+          return (
+            <ExpenceItem
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          );
+        })}
       </Card>
     </div>
   );
