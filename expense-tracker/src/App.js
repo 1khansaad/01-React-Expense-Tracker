5ab3b851470620c779c10b47aabe15e3997f0256
1khansaad <saadkhan8268@gmail.com>
2023-02-01 20:36:11 +0530

    updated state after updating array of expenses

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,37 +1,42 @@
+import React, { useState } from "react";
 import Expenses from "./components/expenses/Expenses.js";
 import NewExpense from "./components/NewExpense/NewExpense.js";
 
+const dummyData = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 1,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: "e2",
+    title: "New TV (Curve)",
+    amount: 2,
+    date: new Date(2021, 2, 12),
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 3,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 4,
+    date: new Date(2021, 5, 12),
+  },
+];
+
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    {
-      id: "e2",
-      title: "New TV (Curve)",
-      amount: 799.49,
-      date: new Date(2021, 2, 12),
-    },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450.6,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expenses, newExpenseList] = useState(dummyData);
 
   const onExpenseDataTransfer = (expenseData) => {
-    expenses.push(expenseData);
-    console.log(expenses);
+    newExpenseList((expense) => {
+      console.log([expenseData, ...expense]);
+      return [expenseData, ...expense];
+    });
   };
   return (
     <div>
--- expense-tracker/src/components/expenses/ExpenceItem.js
+++ expense-tracker/src/components/expenses/ExpenceItem.js
@@ -4,16 +4,11 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Crad";
 
 function ExpenceItem(props) {
-  const [currentTitle, titleFunction] = useState(props.title);
-
-  const eventHandler = () => {
-    titleFunction("Updated!");
-  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{currentTitle}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}$</div>
       </div>
     </Card>
